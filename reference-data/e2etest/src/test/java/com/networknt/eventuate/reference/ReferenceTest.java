0ac6911708260e025b4782b0698949c51cf0ac0c
Gavin Chen <chenga@Gavins-MacBook-Pro.local>
2017-05-18 15:43:55 -0400

    add e2e module

---
+++ reference-data/e2etest/src/test/java/com/networknt/eventuate/reference/ReferenceTest.java
@@ -0,0 +1,96 @@
+package com.networknt.eventuate.reference;
+
+import com.networknt.eventuate.common.AggregateRepository;
+import com.networknt.eventuate.common.EventuateAggregateStore;
+
+
+import com.networknt.eventuate.reference.common.model.ReferenceData;
+import com.networknt.eventuate.reference.domain.ReferenceDataAggregate;
+
+import com.networknt.service.SingletonServiceFactory;
+
+import org.h2.tools.RunScript;
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+* implement end to end test for reference data event process
+*/
+public class ReferenceTest {
+
+    static final Logger logger = LoggerFactory.getLogger(ReferenceTest.class);
+
+    public static DataSource ds;
+
+    static {
+        ds = (DataSource) SingletonServiceFactory.getBean(DataSource.class);
+        try (Connection connection = ds.getConnection()) {
+            // Runscript doesn't work need to execute batch here.
+            String schemaResourceName = "/embedded-event-store-schema.sql";
+            InputStream in = ReferenceTest.class.getResourceAsStream(schemaResourceName);
+
+            if (in == null) {
+                throw new RuntimeException("Failed to load resource: " + schemaResourceName);
+            }
+            InputStreamReader reader = new InputStreamReader(in);
+            RunScript.execute(connection, reader);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private EventuateAggregateStore eventStore  = (EventuateAggregateStore)SingletonServiceFactory.getBean(EventuateAggregateStore.class);
+
+    private AggregateRepository refRepository = new AggregateRepository(ReferenceDataAggregate.class, eventStore);
+
+    private ReferenceCommandServiceImpl  service = new ReferenceCommandServiceImpl(refRepository);
+
+    @Test
+    public void testAddRed() throws Exception {
+
+        ReferenceData ref = new ReferenceData();
+        ref.setReferenceName("country");
+        CompletableFuture<ReferenceData> result = service.add(ref).thenApply((e) -> {
+            ReferenceData m = e.getAggregate().getReferenceData();
+            System.out.println("m = " + m);
+            System.out.println("m = " + e.getEntityId());
+            return m;
+        });
+
+        System.out.println("result = " + result.get());
+    }
+
+    @Test
+    public void testUpdateRef() throws Exception {
+
+        ReferenceData ref = new ReferenceData();
+        ref.setReferenceName("country");
+
+        CompletableFuture<String > id  = service.add(ref).thenApply((e) -> {
+            String m =  e.getEntityId();
+            return m;
+        });
+
+        ref.setActive(false);
+        CompletableFuture<ReferenceData> result = service.update(id.get(), ref).thenApply((e) -> {
+            ReferenceData m = e.getAggregate().getReferenceData();
+            System.out.println("m = " + m);
+            System.out.println("m = " + e.getEntityId());
+            return m;
+        });
+
+        System.out.println("result = " + result.get());
+    }
+}
